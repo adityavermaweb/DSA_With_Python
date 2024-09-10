93428270c408a6fa31fe6e1090f66a95adb4244a
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2024-09-10 17:01:21 +0000

    container config done

---
+++ Assign3.py
@@ -0,0 +1,12 @@
+# 1) Define a class Node to describr a node of a singly linked list.
+# 2) Define a class SLL to implement Singly Linked List with __init__() method to crrate and initialise start reference variable.
+# 3) Define a method is_empty() to check if the linked list is empty in SLL class.
+# 4) In class SLL, define a method insert_at_start() to insert an element at the starting of the list.
+# 5) In class SLL, define a method insert_at_last() to insert an element at the end of the list.
+# 6) In class SLL, define a method search() to find the node with specified element value.
+# 7) In class SLL, define a method insert_after() to insert a new node after a given node of the list.
+# 8) In class SLL, define a method to print all the elements of the list.
+# 9) In class SLL, implement iterator for SLL to access all the elements of the list in a sequence.
+# 10) In class SLL, define a method delete_first() to delete first element from the list.
+# 11) In class SLL, define a method delete_last() to delete last element from the list.
+# 12) In class SLL, define a method delete_item() to delete specified element from the list. 
