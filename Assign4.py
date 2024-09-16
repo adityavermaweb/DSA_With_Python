8f2573ca7eca71133e0cc053d261fdef3cfc4a36
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2024-09-16 11:04:17 +0000

    container config done

--- Assign4.py
+++ Assign4.py
@@ -1,1 +1,14 @@
 # --------------Question----------------------->
+
+# 1) Define a class Node to describe a node of a double linked list ?
+# 2) Define a class DLL to implement Doubly Linked List with __init__() method to create and initialise start reference variable ?
+# 3) Define a method is__empty() to check if the linked list is empty in DLL class ?
+# 4) In class DLL, define a method insert_at_start() to insert an element at the starting of the list ?
+# 5) In class DLL, define a method insert_at_last() to insert an element at the end of the list ?
+# 6) In class DLL, define a method search() to find the node with specified element value ?
+# 7) In class DLL, define a method insert_after() to insert a new node after a given node of the list ?
+# 8) In class DLL, define a method to print all the elements of the list ?
+# 9) In class DLL, implement iterator for DLL to access all the elements of the list in a sequence ?
+# 10) In class DLL, define a method delete_first() to delete first element from the list ?
+# 11) In class DLL, define a method delete_last() to delete last element from the list ?
+# 12) In class DLL, define a method delete_item() to delete specified element from the list ?
